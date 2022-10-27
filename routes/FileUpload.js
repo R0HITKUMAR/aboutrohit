84a4c45a2fac5034d044261d0876e2e767fba251
Rohit Kumar <r.k2962002@gmail.com>
2022-10-27 21:56:44 +0530

    #

--- routes/FileUpload.js
+++ routes/FileUpload.js
@@ -21,9 +21,9 @@ rohitServer.post("/upload", upload.single("file"), function (req, res, next) {
 
     // Upload file to S3
     const s3 = new AWS.S3({
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-      region: process.env.AWS_REGION,
+      accessKeyId: process.env.AWS1_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS1_SECRET_ACCESS_KEY,
+      region: process.env.AWS1_REGION,
     });
 
     s3.upload({
